11845b7707af7c8677b4f95ad8ea6525dcdb7ba0
codewithnimraa <136242525+codewithnimraa@users.noreply.github.com>
2023-07-10 15:41:44 +0500

    Add files via upload

--- function.dart
+++ function.dart
@@ -1,31 +1,11 @@
 void main() {
-  markSheet(
-      name: "nimra",
-      rollno: "34",
-      standard: "2",
-      eng: "88",
-      urdu: "56",
-      math: "99");
-    
+  printTable(10);
+  print("*****************");
+  printTable(11);
 }
 
-markSheet(
-    {required name,
-    required rollno,
-    required standard,
-    required eng,
-    required urdu,
-    required math}) {
-  var eng = 98;
-  print("Eng= $eng");
-  var urdu = 88;
-  print("Urdu =$urdu");
-  var math = 78;
-  print("Math =$math");
-  var obtainMarks = eng + urdu + math;
-  print("obtainMarks = $obtainMarks");
-  var totalMarks = 300;
-  print("TotalMarks = $totalMarks");
-  var per = obtainMarks * 100 / totalMarks;
-  print("percentage = $per");
+printTable(table) {
+  for (var i = 1; i <= 10; i++) {
+    print("$table x $i = ${table * i}");
+  }
 }
